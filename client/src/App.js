499e3b46fa8a753c8c4f4b47c90f8a66d2666697
tothviktoria <viktoriatoth.urban.couture@gmail.com>
2020-02-21 11:16:04 +0100

    sandra mvp doing

--- client/src/App.js
+++ client/src/App.js
@@ -14,25 +14,49 @@ import About from "./components/About";
 import Home from "./components/Home";
 import Sign from "./components/Sign";
 import Collection from "./components/Collection";
+import "./App.css";
 
 export default function App() {
   return (
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">Our story</Link>
-          </li>
-          <li>
-            <Link to="/vinyls">Vinyls</Link>
-          </li>
-          <li>
-            <Link to="/profile">My profile</Link>
-          </li>
-        </ul>
+        <div class="row">
+          <div class="col-sm p-5 bg-warning">
+            <div class="d-flex justify-content-center text-white text-left">
+              VinylSwap
+            </div>
+            <div>
+              <div class="col-sm p-4 bg-dark">
+                <div class="d-flex justify-content-center text-white text-left">
+                  <Link to="/home">HOME</Link>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-sm p-4 bg-light">
+              <div class="d-flex justify-content-center text-muted text-left">
+                <Link to="/about">OUR STORY</Link>
+              </div>
+            </div>
+
+            <div class="col-sm p-4 bg-dark">
+              <div class="d-flex justify-content-center text-white text-left">
+                <Link to="/profile">My profile</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <section class="mainimage">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-1 pg-5"></div>
+              <div class="d-flex justify-content-right text-dark text-right">
+                <Link to="/vinyls">Vinyls</Link>
+              </div>
+            </div>
+          </div>
+        </section>
 
         <Switch>
           <Route path="/about">
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Sign from "./Sign";
+
 export default class Home extends Component {
   render() {
-    return (
-      <div>
-        <h1>Welcome to VinylSwap!</h1>
-        <h2> Still basic, but quite fantastic! </h2>
-        <div>
-          <Sign />
-        </div>
-      </div>
-    );
+    return <div></div>;
   }
 }
