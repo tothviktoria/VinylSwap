fb8558da32c901c2e0a8a933b6f2bd3c728e74f3
tothviktoria <viktoriatoth.urban.couture@gmail.com>
2020-02-21 16:57:32 +0100

    sandramvp doing

--- client/src/App.js
+++ client/src/App.js
@@ -21,10 +21,10 @@ export default function App() {
     <Router>
       <div>
         <div class="row">
-          <div class="col-sm-12 p-5 bg-warning">
+          <div class="col-sm-12 px-5 py-2 p bg-warning">
             <div class="d-flex justify-content-right text-white"></div>
             <div>
-              <div class="col-sm-2 p-4 shadow-lg p-3 mb-5 bg-white rounded">
+              <div class="col-sm-2 px-3 py-2 shadow-lg p-3 mb-5 bg-white rounded">
                 <div class="d-flex justify-content-center">
                   <Link class="text-decoration-none text-warning" to="/home">
                     HOME
@@ -33,7 +33,7 @@ export default function App() {
               </div>
             </div>
 
-            <div class="col-sm-2 p-4 shadow-lg p-3 mb-5 bg-white rounded">
+            <div class="col-sm-2 px-3 py-2 shadow-lg p-3 mb-5 bg-white rounded">
               <div class="d-flex justify-content-center">
                 <Link class="text-decoration-none text-warning" to="/about">
                   OUR STORY
@@ -41,31 +41,26 @@ export default function App() {
               </div>
             </div>
 
-            <div class="col-sm-2 p-4 shadow-lg p-3 mb-5 bg-white rounded">
+            <div class="col-sm-2 px-3 py-2  shadow-lg p-3 mb-5 bg-white rounded">
               <div href="#" class="d-flex justify-content-center">
                 <Link class="text-decoration-none text-warning" to="/profile">
                   MY PROFILE
                 </Link>
               </div>
             </div>
-            <div class="row">
-              <div class="col-4 pg-5">VinylSwap</div>
-            </div>
           </div>
         </div>
 
         <section class="mainimage">
-          <div class="container">
-            <div class="row">
-              <div class="col-sm-1 pg-4"></div>
-              <div class="d-flex justify-content-center">
-                <Link
-                  class="btn btn-white shadow-lg p-3 mb-5 bg-white rounded text-decoration-none text-warning"
-                  to="/vinyls"
-                >
-                  VINYLS
-                </Link>
-              </div>
+          <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="d-flex justify-content-center">
+              <Link
+                class="btn btn-white shadow-lg px-3 py-2 bg-white rounded text-decoration-none text-warning"
+                to="/vinyls"
+              >
+                VINYLS
+              </Link>
             </div>
           </div>
         </section>
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -3,22 +3,60 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import MyVinyls from "./MyVinyls";
 import Account from "./Account";
 import Inbox from "./Inbox";
+import "./Profile.css";
 
 export default class Profile extends Component {
   render() {
     return (
       <div>
-        <ul>
-          <button type="button" class="btn btn-dark">
-            <Link to={`${this.props.match.url}/myVinyls`}>My box</Link>
-          </button>
-          <button type="button" class="btn btn-dark">
-            <Link to={`${this.props.match.url}/account`}> My account</Link>
-          </button>
-          <button type="button" class="btn btn-dark">
-            <Link to={`${this.props.match.url}/inbox`}> Inbox</Link>
-          </button>
-        </ul>
+        <section class="thirdpart">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 p-5"></div>
+              <div>
+                <div class="d-flex justify-content-center">
+                  <div class="col-sm-12 px-3 py-2"></div>
+                  <button
+                    type="button"
+                    class="btn btn-white shadow-lg px-5 py-2 bg-white rounded btn-space"
+                  >
+                    <Link
+                      class="text-decoration-none text-muted"
+                      to={`${this.props.match.url}/myVinyls`}
+                    >
+                      My box
+                    </Link>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-white shadow-lg px-5 py-2 bg-white rounded btn-space"
+                  >
+                    <Link
+                      class="text-decoration-none text-muted"
+                      to={`${this.props.match.url}/account`}
+                    >
+                      {" "}
+                      My account
+                    </Link>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-white shadow-lg px-5 py-2 bg-white rounded btn-space"
+                  >
+                    <Link
+                      class="text-decoration-none text-muted"
+                      to={`${this.props.match.url}/inbox`}
+                    >
+                      {" "}
+                      Inbox
+                    </Link>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
         <Switch>
           <Route path={`${this.props.match.path}/myVinyls`}>
             <MyVinyls />
